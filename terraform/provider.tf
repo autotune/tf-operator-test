08ed1f06c7a774d2e3150728b82f63b1e6beb74a
ExperiencedDevOps <service@contrasting.org>
2022-10-29 10:40:33 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,13 @@
 terraform {
-  backend "s3" {
-    key                         = "prod.tfstate"
-    bucket                      = "wayofthesys"
-    region                      = "us-east-2"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.27.0"
+    }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
